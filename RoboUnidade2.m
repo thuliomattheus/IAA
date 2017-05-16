d582a5ec80fd6450f0cef81fb702bdc6602e42da
travesseiro3 <thulio_fla@hotmail.com>
2017-05-16 18:20:45 -0300

    Add files via upload

--- RoboUnidade2.m
+++ RoboUnidade2.m
@@ -1,30 +1,26 @@
 % Implementação da movimentação de um robo treinado por uma MLP
-% Autor : Alex Alves de Medeiros
-%         Thúlio Mattheus Pereira de Sousa
+% Autores : Alex Alves de Medeiros
+%           Thúlio Mattheus Pereira de Sousa
 
 function [ ] = RoboUnidade2( serPort )
+
+    clc;
+
     % Tempo máximo de simulação
 	DuracaoMaxima = 1200;
     % Marcar o inicio do tempo
     inicio = tic;
-    % Velocidade  (m/s)
-%    velocidade= 0.5;
-
-%    r = 0.16; %raio do robo
-%    L = 0.258; %distancia entre as rodas
 
     SetFwdVelAngVelCreate(serPort,0,0) %Inicia o robo
 
 
 
-
-
-
-
-%    javaaddpath('C:\Program Files\swipl\lib\jpl.jar');
+%   javaaddpath('C:\Program Files\swipl\lib\jpl.jar');
     x = org.jpl7.Query('consult(''testeROBO.pl'')');
     x.hasSolution;
 
+    iteracao = 0;
+    
     while toc(inicio) < DuracaoMaxima
 
         % 1 right sonar, 2 – front, 3 – left, 4 – back.
@@ -77,21 +73,16 @@ function [ ] = RoboUnidade2( serPort )
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%        VelocidadeRodaEsquerda = 0.3;
-%        VelocidadeRodaDireita = 0;
+        [a, b] = genOverhead(serPort);
+        plot(a, b, 'b*');
+
+%        fprintf('Eixo X = %.1f   Eixo Y = %.1f\n\n', a, b);        
         fprintf('Frente = %.1f   Direita = %.1f   Esquerda = %.1f   Tras = %.1f\n\n', Frente, Direita, Esquerda, Tras);
         fprintf('Roda da Esquerda = %.1f\nRoda da Direita = %.1f\n\n', VelocidadeRodaEsquerda, VelocidadeRodaDireita);
-        
-        
-        % Calculando as velocidades angulares 
-%        we = VelocidadeRodaEsquerda/r;
-%        wd = VelocidadeRodaDireita/r;
-        % Faz o robo andar a partir das velocidades lineares das rodas
-        SetDriveWheelsCreate(serPort,VelocidadeRodaDireita,VelocidadeRodaEsquerda)
-        % Movimenta através das velocidades angulares
-        % SetDriveWheelsCreate(serPort,wd,we); 
 
-%        velocidade = (r/2)*(wd+we);
-%        w = (r/L)*(wd-we);     
+        SetDriveWheelsCreate(serPort,VelocidadeRodaDireita,VelocidadeRodaEsquerda);
+        
+        iteracao = iteracao + 1;
+        
     end
 end
